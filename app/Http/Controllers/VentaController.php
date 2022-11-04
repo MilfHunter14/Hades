08edba366c3d1b79b1ffd4fb5192a7f87a86af84
Blackhand14 <fercho550.medina@gmail.com>
2022-11-03 21:47:21 -0600

    MIDDLEWARE_DISEÑO

--- app/Http/Controllers/VentaController.php
+++ app/Http/Controllers/VentaController.php
@@ -9,6 +9,10 @@ use Illuminate\Http\Request;
 
 class VentaController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth')->except('index', 'show');
+    }
     /**
      * Display a listing of the resource.
      *
--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,4 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
