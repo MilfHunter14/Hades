cb8eb76da81bf4cbf7008e0275729b1e509f8ea7
Angel Diaz <76696590+anngeldiaz24@users.noreply.github.com>
2022-11-12 10:05:01 -0600

    PROYECTO 2
    Cruds completos con relaciones 1:M, N:M, uso de sesiones

--- resources/views/empleados/empleadosEdit.blade.php
+++ resources/views/empleados/empleadosEdit.blade.php
@@ -49,7 +49,7 @@
 
                                 <div class="form-outline mb-4">
                                 <label class="form-label" for="telefono">Teléfono: </label></br>
-                                <input type="text" class="form-control form-control-lg" name="telefono" id="telefono" autocomplete="off" min=10 max=10 required value="{{ old('telefono') ?? $empleado->telefono }}">
+                                <input type="text" class="form-control form-control-lg" name="telefono" id="telefono" autocomplete="off" min=10 max=10 required value="{{ old('telefono') ?? $empleado->telefono }}" maxlength="10">
                                 @error('telefono')
                                     <i>{{ $message}}</i>
                                 @enderror
